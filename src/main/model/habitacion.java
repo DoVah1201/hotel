05af3e5ee730d905e113b6625eb1a8f70a34ef3e
Marisapr2006 <marisapenarey@gmail.com>
2025-04-01 22:28:05 +0100

    Update habitacion.java
    si

--- src/main/model/habitacion.java
+++ src/main/model/habitacion.java
@@ -1,5 +1,23 @@
 package src.main.model;
 
 public class habitacion {
+    private int numHab;
+    private tipoHab tipo;
+    private estadoHab estado;
+    private String descripcion;
+
+    public Habitacion(int numHab, tipoHab tipo, String descripcion) {
+        this.numHab = numHab;
+        this.tipo = tipo;
+        this.estado = estadoHab.DISPONIBLE;
+        this.descripcion = descripcion;
+    }
     
+    public tipoHab getTipo() {
+        return tipo;
+    }
+
+    public String toString() {
+        return "Habitación" + numHab + " - " + tipo + " (" + estado + ") ";
+    }
 }
