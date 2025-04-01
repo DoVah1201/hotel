dfb1d363b3be8f6098a48297d3a12f177abaf8d0
Saul <123548756+DoVah1201@users.noreply.github.com>
2025-04-01 22:13:54 +0100

    enums
    poniendo a punto los enums de tipohabitacion

--- src/main/model/tipohabitacion.java
+++ src/main/model/tipohabitacion.java
@@ -2,8 +2,17 @@ package src.main.model;
 
 public enum tipohabitacion {
     
-    INDIVIDUAL,
-    DOBLE,
-    SUITE
+        INDIVIDUAL(50), DOBLE(80), SUITE(150);
+        
+        private double precioPorNoche;
+        
+         tipohabitacion(double precio) {
+            this.precioPorNoche = precio;
+        }
+        
+        public double getPrecioPorNoche() {
+            return precioPorNoche;
+        }
+    }
 
-}
+
