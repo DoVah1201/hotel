312e788ffe2d301c941d46a20e484823dccbd8e8
Saul <thesimulgamer007@gmail.com>
2025-04-02 20:14:23 +0100

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void hotel(String[] args) {
 
         GestorHotel controlador = new GestorHotel();
         vistahotel vista = new vistahotel();
